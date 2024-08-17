9402328404097fb9e3cfb23693a074d76b74de77
packy <pli791@aucklanduni.ac.nz>
2024-08-17 21:00:38 +1200

    Add new feature Replay System

---
+++ Assets/Code/Scripts/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+   
+    public GameObject selectedPlayer;
+    
+
+    public void StartRecording()
+    {
+
+        ResetPlayer();
+        selectedPlayer.GetComponent<ActorObject>().Recording();
+    }
+
+    public void StartPlayback()
+    {
+        ResetPlayer();
+        selectedPlayer.GetComponent<ActorObject>().Playback();
+    }
+
+    public void ResetPlayer()
+    {
+        selectedPlayer.GetComponent<ActorObject>().Reset();
+
+    }
+
+ }
+
