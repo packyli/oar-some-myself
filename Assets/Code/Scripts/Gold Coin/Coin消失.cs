c25a80b443301e0f1ef25670d86f4f6bda05d431
jyan605 <jyan605@aucklanduni.ac.nz>
2024-08-23 23:13:23 +1200

    coin 测试版
    建模，贴图，转动，鼠标指向

---
+++ Assets/Code/Scripts/Gold Coin/Coin消失.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin消失 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    
+    // 反馈材料
+    public Material highlightMaterial;
+
+    private Material originalMaterial;
+    private GameObject lastHighlighted;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            var target = hit.collider.gameObject;
+            if (lastHighlighted != null && lastHighlighted != target)
+            {
+                // 如果鼠标不再悬停在原对象上，恢复原材料
+                ResetHighlight();
+            }
+
+            if (lastHighlighted != target)
+            {
+                // 保存原始材料
+                originalMaterial = target.GetComponent<Renderer>().material;
+                // 改变材料以显示高亮
+                target.GetComponent<Renderer>().material = highlightMaterial;
+                lastHighlighted = target;
+            }
+        }
+        else
+        {
+            // 如果没有物体被射线投中，恢复上一个高亮的物体的材料
+            ResetHighlight();
+        }
+    }
+
+    void ResetHighlight()
+    {
+        if (lastHighlighted != null)
+        {
+            lastHighlighted.GetComponent<Renderer>().material = originalMaterial;
+            lastHighlighted = null;
+        }
+    }
+}
+
