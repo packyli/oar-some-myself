87ab0fc4b074790fd98cbbf143f01bb79fa20442
jyan605 <jyan605@aucklanduni.ac.nz>
2024-09-12 15:21:36 +1200

    眼球追踪上传

--- Assets/Code/Scripts/Gold Coin/CoinVanish.cs
+++ Assets/Code/Scripts/Gold Coin/CoinVanish.cs
@@ -15,23 +15,55 @@ public class CoinVanish : MonoBehaviour
     private Material originalMaterial;
     private GameObject lastHighlighted;
 
+    public float requiredGazeTime = 0.5f;  // 需要凝视的时间长度（秒）
+    private float gazeTimer = 0.0f;  // 凝视计时器
+    private Transform lastGazedUpon;  // 上一次凝视的物体
+    private bool gameStarted = false;  // 游戏是否已经开始
+
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            var target = hit.collider.gameObject;
-            
+            gameStarted = true;
+        }
 
-            if ( hit.transform.CompareTag("Animal"))
+        if (gameStarted)  // 只有在游戏开始后才执行以下逻辑
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
             {
-                target.gameObject.SetActive(false);
-               
+                var target = hit.collider.gameObject;
+
+
+                if (hit.transform.CompareTag("Animal"))
+                {
+                    target.gameObject.SetActive(false);
+
+                }
+            }
+            Transform gazedObject = GetGazedObject();
+            if (gazedObject != null && gazedObject.CompareTag("Animal"))
+            {
+                if (lastGazedUpon != gazedObject)
+                {
+                    lastGazedUpon = gazedObject;
+                    gazeTimer = 0.0f;
+                }
+
+                gazeTimer += Time.deltaTime;
+                if (gazeTimer >= requiredGazeTime)
+                {
+                    gazedObject.gameObject.SetActive(false);  // 使物体消失
+                }
+            }
+            else
+            {
+                gazeTimer = 0.0f;  // 重置计时器
+                lastGazedUpon = null;
             }
         }
-        
     }
 
     void ResetHighlight()
@@ -41,6 +73,17 @@ public class CoinVanish : MonoBehaviour
             lastHighlighted.GetComponent<Renderer>().material = originalMaterial;
             lastHighlighted = null;
         }
+    }
+    Transform GetGazedObject()
+    {
+        Ray centerEyeRay = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(centerEyeRay, out hit))
+        {
+            return hit.transform;  // 返回凝视的物体
+        }
+        return null;
     }
 }
 
