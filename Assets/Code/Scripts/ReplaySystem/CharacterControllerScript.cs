02fc26ba1dafec928cfde4cf30a5fa0d2395ba24
packy <pli791@aucklanduni.ac.nz>
2024-09-25 19:31:56 +1200

    Update CharacterControllerScripts to remove redundant lines

--- Assets/Code/Scripts/ReplaySystem/CharacterControllerScript.cs
+++ Assets/Code/Scripts/ReplaySystem/CharacterControllerScript.cs
@@ -10,20 +10,15 @@ public class CharacterControllerScript : MonoBehaviour
     public float moveSpeedFactor = 5f;
     public float playerPowerFactor = 1f;
 
-    private GameObject characterGameObject;
-    private GameObject characterBody;
     private CharacterController charCont;
     private Vector3 initialPosition;
     private float horizontalValue;
     private float verticalValue;
     private bool buttonValue;
     private float currentForce;
-    public float baseAmplitude = 1f;
 
     private void Awake()
     {
-        characterGameObject = gameObject;
-        characterBody = GameObject.FindWithTag("PlayerAvatarBody");
         charCont = GetComponent<CharacterController>();
     }
 
@@ -43,7 +38,6 @@ public class CharacterControllerScript : MonoBehaviour
             //The button press has been received, row once
             _animator.SetTrigger("IsRow");
             AlterRowPower();
-            AlterRowAmplitude();
         }
         
         //Actual Character Movement
@@ -57,24 +51,6 @@ public class CharacterControllerScript : MonoBehaviour
         Debug.Log("Rowing Player Avatar's current animator speed: " + _animator.speed);
     }
 
-    public void AlterRowAmplitude()
-    {
-        //// Modify size based on rowing power
-        //float sizeFactor = baseAmplitude * currentForce * playerPowerFactor;
-        //characterBody.transform.localScale = new Vector3(sizeFactor, sizeFactor, sizeFactor);
-        //Debug.Log("Character body size adjusted to: " + sizeFactor);
-
-        //// Get the Renderer component to modify the color
-        //Renderer bodyRenderer = characterBody.GetComponent<Renderer>();
-        //if (bodyRenderer != null)
-        //{
-        //    // Change the color based on power. Here, we're transitioning from blue to red as power increases.
-        //    Color newColor = Color.Lerp(Color.blue, Color.red, currentForce);
-        //    bodyRenderer.material.color = newColor;
-
-        //    Debug.Log("Character body color adjusted to: " + newColor);
-        //}
-    }
 
     public void GivenInputs(PlayerInputStruct Inputs)
     {
