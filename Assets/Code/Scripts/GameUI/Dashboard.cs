f70c5163e6137219d519cd6671e121e949159dc6
packy <pli791@aucklanduni.ac.nz>
2024-10-03 11:06:37 +1300

    Removed redundant debug lines with Chinese characters in Dashbaord.cs

--- Assets/Code/Scripts/GameUI/Dashboard.cs
+++ Assets/Code/Scripts/GameUI/Dashboard.cs
@@ -1,17 +1,12 @@
-
-using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.UI;
-using static RoundController;
 
 public class Dashboard : MonoBehaviour
 {
-
     public uint torsioninput = 0;
     public uint freqinput = 0;
     public float speedinput = 0;
     public uint distance = 0;
-
     public Image torsionImage;
     public Image freqImage;
     public Image speedImage;
@@ -20,6 +15,7 @@ public class Dashboard : MonoBehaviour
     public float maxSpeed = 20f;
     public Engine engine;
     public RoundController roundController;
+
     private AvatarController avatarController;
     private int dropdownValue2;
     private int dropdownValue3;
@@ -32,11 +28,10 @@ public class Dashboard : MonoBehaviour
         engine = FindObjectOfType<Engine>();
         roundController = FindObjectOfType<RoundController>();
         avatarController = FindObjectOfType<AvatarController>();
-
     }
+    
     void Update()
     {
-
         GameObject dropdownObject2 = GameObject.FindGameObjectWithTag("Round2Config");
         GameObject dropdownObject3 = GameObject.FindGameObjectWithTag("Round3Config");
         GameObject dropdownObject4 = GameObject.FindGameObjectWithTag("Round4Config");
@@ -44,44 +39,34 @@ public class Dashboard : MonoBehaviour
 
         if (dropdownObject2 != null && dropdownObject3 != null && dropdownObject4 != null && dropdownObject5 != null)
         {
-            Debug.Log("dropdownValue2 is working ？？？？？？？？？？？？？" + dropdownValue2);
             Dropdown dropdown2 = dropdownObject2.GetComponent<Dropdown>();
             Dropdown dropdown3 = dropdownObject3.GetComponent<Dropdown>();
             Dropdown dropdown4 = dropdownObject4.GetComponent<Dropdown>();
             Dropdown dropdown5 = dropdownObject5.GetComponent<Dropdown>();
 
-
             if (dropdown2 != null && dropdown3 != null && dropdown4 != null && dropdown5 != null)
             {
-
                 dropdownValue2 = dropdown2.value;
                 dropdownValue3 = dropdown3.value;
                 dropdownValue4 = dropdown4.value;
-                dropdownValue5 = dropdown5.value;
-
-                Debug.Log("dropdownValue2 is working !!!!!!!!!!!!!!!!!!!!!!!!" + dropdownValue2);
-                Debug.Log("dropdownValue3 is working !!!!!!!!!!!!!!!!!!!!!!!!" + dropdownValue3);
-                Debug.Log("dropdownValue4 is working !!!!!!!!!!!!!!!!!!!!!!!!" + dropdownValue4);
-                Debug.Log("dropdownValue5 is working !!!!!!!!!!!!!!!!!!!!!!!!" + dropdownValue5);
-
-                
+                dropdownValue5 = dropdown5.value;  
             }
             else
             {
-                Debug.LogError("未找到 Dropdown 组件!!!!!!!!!!!!！");
+                Debug.LogError("Dropdown not found or is null!");
             }
         }
         else
         {
-            Debug.LogError("未找到具有指定 Tag 的对象！!!!!!!!!!!!!!!!!!!!!!!");
+            Debug.LogError("GameObject with specified tag Round2Config/Round3Config/Round4Config/Round5Config not found!");
         }
     }
+
     public void UpdateAvtarBar(PlayerInputStruct recordedInputs)
     {
         int currentRound = engine.currentRound;
         torsioninput = recordedInputs.rowPowerInput;
         float torsionvalue = (float)torsioninput / maxTorsion;
-
 
         freqinput = recordedInputs.rowFrequencyInput;
         float freqvalue = (float)freqinput / maxFreq;
@@ -92,31 +77,19 @@ public class Dashboard : MonoBehaviour
         float PowerFactor = roundController.PowerFactor;
         float FrequencyFactor = roundController.FrequencyFactor;
         float SpeedFactor = roundController.SpeedFactor;
-
-
-
-
         float time = engine.timeCount;
-
-
-
-
 
         if (currentRound == 2)
         {
-            Debug.Log("currentRound ========== 2222222222222222  !!!!!!!!!!!!!!!!!!!!!!!!");
             switch (dropdownValue2)
             {
                 case (int)RoundController.RoundType.OnlyFrequencyChanged:
-                    Debug.Log("freq ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     freqvalue *= FrequencyFactor;
                     break;
                 case (int)RoundController.RoundType.OnlyPowerChanged:
-                    Debug.Log("power ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     break;
                 case (int)RoundController.RoundType.AllChanged:
-                    Debug.Log("all ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     freqvalue *= FrequencyFactor;
                     break;
@@ -124,40 +97,31 @@ public class Dashboard : MonoBehaviour
         }
         else if (currentRound == 3)
         {
-            Debug.Log("currentRound ========== 33333333333  !!!!!!!!!!!!!!!!!!!!!!!!");
             switch (dropdownValue3)
             {
                 case (int)RoundController.RoundType.OnlyFrequencyChanged:
-                    Debug.Log("freq ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     freqvalue *= FrequencyFactor;
                     break;
                 case (int)RoundController.RoundType.OnlyPowerChanged:
-                    Debug.Log("power ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     break;
                 case (int)RoundController.RoundType.AllChanged:
-                    Debug.Log("all ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     freqvalue *= FrequencyFactor;
                     break;
             }
         }
-
         else if (currentRound == 4)
         {
-            Debug.Log("currentRound ========== 444444444444444  !!!!!!!!!!!!!!!!!!!!!!!!");
             switch (dropdownValue4)
             {
                 case (int)RoundController.RoundType.OnlyFrequencyChanged:
-                    Debug.Log("freq ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     freqvalue *= FrequencyFactor;
                     break;
                 case (int)RoundController.RoundType.OnlyPowerChanged:
-                    Debug.Log("power ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     break;
                 case (int)RoundController.RoundType.AllChanged:
-                    Debug.Log("all ==========   !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     freqvalue *= FrequencyFactor;
                     break;
@@ -165,34 +129,23 @@ public class Dashboard : MonoBehaviour
         }
         else if (currentRound == 5)
         {
-            Debug.Log("currentRound ========== 45555555555555555  !!!!!!!!!!!!!!!!!!!!!!!!");
             switch (dropdownValue5)
             {
                 case (int)RoundController.RoundType.OnlyFrequencyChanged:
-                    Debug.Log("freq ========== 222222222222222  !!!!!!!!!!!!!!!!!!!!!!!!");
                     freqvalue *= FrequencyFactor;
                     break;
                 case (int)RoundController.RoundType.OnlyPowerChanged:
-                    Debug.Log("power ========== 111111111111111  !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     break;
                 case (int)RoundController.RoundType.AllChanged:
-                    Debug.Log("all ========== 4444444444444444  !!!!!!!!!!!!!!!!!!!!!!!!");
                     torsionvalue *= PowerFactor;
                     freqvalue *= FrequencyFactor;
                     break;
             }
         }
 
-
-
-
-
-
         torsionImage.fillAmount = torsionvalue;
         freqImage.fillAmount = freqvalue;
         speedImage.fillAmount = speedvalue;
     }
 }
-
-
